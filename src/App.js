d0c411e58658739d90b758372f7a7f84ffee5c55
stevenmills <samills0608@gmail.com>
2017-09-15 12:42:19 -0400

    added some new stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TimerContainer from './containers/TimerContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="columns">
+          <div className="col-md-9 centered">
+            <h3>Bloc Time</h3>
+            <TimerContainer />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/containers/TimerContainer.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class TimerContainer extends Component {
+  // Timer logic here
+}
+
+export default TimerContainer;
